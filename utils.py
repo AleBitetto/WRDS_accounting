52c177d88392b1ddda633de7c415b3853ab292ae
AleBitetto <bitettoalessandro@gmail.com>
2022-11-02 18:17:24 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,194 @@
+import pandas as pd
+
+
+def summary_stats(df=None, date_format='D', n_digits=2):
+
+    '''
+      date_format: show dates up to days https://numpy.org/doc/stable/reference/arrays.datetime.html#arrays-dtypes-dateunits
+      n_digits: rounding digits for min, max, mean, ...
+    '''
+
+    import pandas as pd
+    import numpy as np
+    
+    NUMERICS = ['number', 'int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    DATES = ['datetimetz', 'datetime64', 'datetime']
+    CATEGORICALS = ['object']
+    BOOLS = ['bool']
+    all_cols = df.columns
+
+    tot_rows, tot_cols = df.shape
+    pd.set_option('display.max_rows', tot_cols)
+
+    num_cols = df.select_dtypes(include=NUMERICS).columns
+    date_cols = df.select_dtypes(include=DATES).columns
+    cat_cols = df.select_dtypes(include=CATEGORICALS).columns
+    bool_cols = df.select_dtypes(include=BOOLS).columns
+
+    # Numerical stats
+    num_stats = pd.DataFrame(columns=['VARIABLE', 'TYPE', 'OBS', 'UNIQUE', 'NANs', 'INFs', 'ZEROs', 'MIN', 'MAX', 'MEAN',
+                                      'STDDEV', 'MEDIAN', 'PERC1', 'PERC5', 'PERC95', 'PERC99', 'SUM'])
+    for var in num_cols:
+
+        val = df[var].values
+        val = val[~np.isnan(val)]
+        val = val[~np.isinf(val)]
+
+        perc = np.quantile(val, [0.01, 0.05, 0.95, 0.99])
+
+        add_row = pd.DataFrame({'VARIABLE': var,
+                                'TYPE': str(df[var].dtypes),
+                                'OBS': tot_rows,
+                                'UNIQUE': df[var].nunique(),
+                                'NANs': df[var].isna().sum(),
+                                'INFs': np.isinf(df[var].values).sum(),
+                                'ZEROs': sum(df[var] == 0),
+                                'MIN': np.round(val.min(), n_digits),
+                                'MAX': np.round(val.max(), n_digits),
+                                'MEAN': np.round(val.mean(), n_digits),
+                                'STDDEV': np.round(val.std(), n_digits),
+                                'MEDIAN': np.round(np.median(val), n_digits),
+                                'PERC1': np.round(perc[0], n_digits),
+                                'PERC5': np.round(perc[1], n_digits),
+                                'PERC95': np.round(perc[2], n_digits),
+                                'PERC99': np.round(perc[3], n_digits),
+                                'SUM': np.round(val.sum(), n_digits)
+                               }, index = [0])
+
+        num_stats = num_stats.append(add_row)
+
+
+    # Categorical stats
+    cat_stats = pd.DataFrame(columns=['VARIABLE', 'TYPE', 'OBS', 'UNIQUE', 'NANs', 'BLANKs', 'VALUES_BY_FREQ'])
+
+    for var in cat_cols:
+
+        add_row = pd.DataFrame({'VARIABLE': var,
+                                'TYPE': str(df[var].dtypes),
+                                'OBS': tot_rows,
+                                'UNIQUE': df[var].nunique(),
+                                'NANs': df[var].isna().sum(),
+                                'BLANKs': sum(df[var] == ''),
+                                'VALUES_BY_FREQ': '|'.join(df[var].value_counts().index[:40])
+                               }, index = [0])
+
+        cat_stats = cat_stats.append(add_row)
+
+
+    # Boolean stats
+    bool_stats = pd.DataFrame(columns=['VARIABLE', 'TYPE', 'OBS', 'NANs', 'MEAN', 'STDDEV', 'MEDIAN', 'PERC1',
+                                       'PERC5', 'PERC95', 'PERC99', 'SUM', 'VALUES_BY_FREQ'])
+    for var in bool_cols:
+
+        val = df[var].dropna().values
+
+        perc = np.quantile(val.astype(int), [0.01, 0.05, 0.95, 0.99])
+                                                   
+        add_row = pd.DataFrame({'VARIABLE': var,
+                                'TYPE': str(df[var].dtypes),
+                                'OBS': tot_rows,
+                                'NANs': df[var].isna().sum(),
+                                'MEAN': np.round(val.mean(), n_digits),
+                                'STDDEV': np.round(val.std(), n_digits),
+                                'MEDIAN': np.round(np.median(val), n_digits),
+                                'PERC1': perc[0],
+                                'PERC5': perc[1],
+                                'PERC95': perc[2],
+                                'PERC99': perc[3],
+                                'SUM': val.sum(),
+                                'VALUES_BY_FREQ': ', '.join([str(k) + ': ' + str(v)
+                                                             for k, v in df['bool1'].dropna().value_counts().to_dict().items()])
+                               }, index = [0])
+
+        bool_stats = bool_stats.append(add_row)
+
+    # Date stats
+    date_stats = pd.DataFrame(columns=['VARIABLE', 'TYPE', 'OBS', 'UNIQUE', 'NANs', 'MIN', 'MAX', 'MEDIAN', 'PERC1',
+                                       'PERC5', 'PERC95', 'PERC99'])
+    for var in date_cols:
+
+        val = df[var].dropna().values
+        val_str = np.datetime_as_string(val, unit=date_format)
+
+        # calculation for median and quantile
+        val_cnt = sorted(pd.Series(val_str).unique())
+        mapping = dict(zip(val_cnt, range(len(val_cnt))))
+        mapped = [mapping.get(v, v) for v in val_str]
+        med_ind = np.median(mapped).astype(int)
+
+        perc = np.quantile(mapped, [0.01, 0.05, 0.95, 0.99]).astype(int)
+
+        add_row = pd.DataFrame({'VARIABLE': var,
+                                'TYPE': str(df[var].dtypes),
+                                'OBS': tot_rows,
+                                'UNIQUE': df[var].nunique(),
+                                'NANs': df[var].isna().sum(),
+                                'MIN': np.datetime_as_string(val.min(), unit=date_format),
+                                'MAX': np.datetime_as_string(val.max(), unit=date_format),
+                                'MEDIAN': [k for k, v in mapping.items() if v == med_ind][0],
+                                'PERC1': [k for k, v in mapping.items() if v == perc[0]][0],
+                                'PERC5': [k for k, v in mapping.items() if v == perc[1]][0],
+                                'PERC95': [k for k, v in mapping.items() if v == perc[2]][0],
+                                'PERC99': [k for k, v in mapping.items() if v == perc[3]][0]
+                               }, index = [0])
+
+        date_stats = date_stats.append(add_row)
+
+    # final stats
+    all_col_set = ['VARIABLE', 'TYPE', 'OBS', 'UNIQUE', 'NANs', 'INFs', 'ZEROs', 'BLANKs', 'MEAN', 'STDDEV', 'MIN', 
+                                       'PERC1', 'PERC5', 'MEDIAN', 'PERC95', 'PERC99', 'MAX', 'SUM', 'VALUES_BY_FREQ']
+    used_col_set = []
+    final_stats = pd.DataFrame(columns=all_col_set)
+    if num_stats.shape[0] > 0:
+        final_stats = final_stats.append(num_stats)
+        used_col_set.extend(num_stats.columns)
+    if cat_stats.shape[0] > 0:
+        final_stats = final_stats.append(cat_stats)
+        used_col_set.extend(cat_stats.columns)
+    if bool_stats.shape[0] > 0:
+        final_stats = final_stats.append(bool_stats)
+        used_col_set.extend(bool_stats.columns)
+    if date_stats.shape[0] > 0:
+        final_stats = final_stats.append(date_stats)
+        used_col_set.extend(date_stats.columns)
+
+    final_stats = final_stats[[x for x in all_col_set if x in np.unique(used_col_set)]]
+
+    if final_stats['VARIABLE'].nunique() != final_stats.shape[0]:
+        print('-- Duplicated variables found!')
+
+    if final_stats['VARIABLE'].nunique() != tot_cols:
+        print('-- Missing variables found:\n    ', '\n     '.join(set(all_cols) - set(final_stats['VARIABLE'].values)))
+
+    final_stats['order'] = pd.Categorical(final_stats['VARIABLE'], categories = all_cols, ordered = True)
+    final_stats = final_stats.sort_values(by='order').drop(columns=['order'])
+
+    # add percentage to missing, inf, zeros, blank
+    if 'NANs' in final_stats.columns:
+        final_stats['NANs'] = [str(x) + ' (' +  str(np.round(x / tot_rows * 100, 1)) + '%)'
+                               if not np.isnan(x) else '' for x in final_stats['NANs'].values]
+    if 'INFs' in final_stats.columns:
+        final_stats['INFs'] = [str(x) + ' (' +  str(np.round(x / tot_rows * 100, 1)) + '%)'
+                               if not np.isnan(x) else '' for x in final_stats['INFs'].values]
+    if 'ZEROs' in final_stats.columns:
+        final_stats['ZEROs'] = [str(x) + ' (' +  str(np.round(x / tot_rows * 100, 1)) + '%)'
+                               if not np.isnan(x) else '' for x in final_stats['ZEROs'].values]
+    if 'BLANKs' in final_stats.columns:
+        final_stats['BLANKs'] = [str(x) + ' (' +  str(np.round(x / tot_rows * 100, 1)) + '%)'
+                               if not np.isnan(x) else '' for x in final_stats['BLANKs'].values]
+    final_stats.fillna('', inplace=True)
+    
+    return final_stats
+
+
+def stats_with_description(df, df_vardescr_path):
+    
+    df_stats = summary_stats(df)
+    df_vardescr = pd.read_csv(df_vardescr_path, sep=';').drop(columns=['Type'])
+    df_vardescr['Variable Name'] = df_vardescr['Variable Name'].str.lower()
+    df_stats = df_stats.merge(df_vardescr, how='left', left_on='VARIABLE', right_on='Variable Name').drop(columns=['Variable Name'])
+    move_col = df_stats.pop('Description')
+    df_stats.insert(1, 'Description', move_col)
+    df_stats.fillna('', inplace=True)
+    
+    return df_stats
