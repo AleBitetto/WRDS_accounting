df78fb2b60a3ef12edaeedfc295214bd6066b86b
AleBitetto <bitettoalessandro@gmail.com>
2022-11-25 13:36:33 +0800

    all data matched and ready for prediction

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from timeit import default_timer as timer
 import datetime
 import re
 import itertools
+import functools
+import time
 
 
 def summary_stats(df=None, date_format='D', n_digits=2):
@@ -30,6 +32,10 @@ def summary_stats(df=None, date_format='D', n_digits=2):
     cat_cols = df.select_dtypes(include=CATEGORICALS).columns
     bool_cols = df.select_dtypes(include=BOOLS).columns
 
+    # convert int64 to float (so as to evaluate mean, etc)
+    for col in df.select_dtypes(include=['int64']).columns:
+        df[col] = df[col].astype(float)    
+    
     # Numerical stats
     num_stats = pd.DataFrame(columns=['VARIABLE', 'TYPE', 'OBS', 'UNIQUE', 'NANs', 'INFs', 'ZEROs', 'MIN', 'MAX', 'MEAN',
                                       'STDDEV', 'MEDIAN', 'PERC1', 'PERC5', 'PERC95', 'PERC99', 'SUM'])
@@ -74,7 +80,7 @@ def summary_stats(df=None, date_format='D', n_digits=2):
                                 'UNIQUE': df[var].nunique(),
                                 'NANs': df[var].isna().sum(),
                                 'BLANKs': sum(df[var] == ''),
-                                'VALUES_BY_FREQ': '|'.join(df[var].value_counts().index[:40])
+                                'VALUES_BY_FREQ': '|'.join(df[var].value_counts().index[:40].astype(str))
                                }, index = [0])
 
         cat_stats = cat_stats.append(add_row)
@@ -121,21 +127,29 @@ def summary_stats(df=None, date_format='D', n_digits=2):
         mapped = [mapping.get(v, v) for v in val_str]
         med_ind = np.median(mapped).astype(int)
 
-        perc = np.quantile(mapped, [0.01, 0.05, 0.95, 0.99]).astype(int)
-
-        add_row = pd.DataFrame({'VARIABLE': var,
-                                'TYPE': str(df[var].dtypes),
-                                'OBS': tot_rows,
-                                'UNIQUE': df[var].nunique(),
-                                'NANs': df[var].isna().sum(),
-                                'MIN': np.datetime_as_string(val.min(), unit=date_format),
-                                'MAX': np.datetime_as_string(val.max(), unit=date_format),
-                                'MEDIAN': [k for k, v in mapping.items() if v == med_ind][0],
-                                'PERC1': [k for k, v in mapping.items() if v == perc[0]][0],
-                                'PERC5': [k for k, v in mapping.items() if v == perc[1]][0],
-                                'PERC95': [k for k, v in mapping.items() if v == perc[2]][0],
-                                'PERC99': [k for k, v in mapping.items() if v == perc[3]][0]
-                               }, index = [0])
+        if len(val) > 0:
+        
+            perc = np.quantile(mapped, [0.01, 0.05, 0.95, 0.99]).astype(int)
+            add_row = pd.DataFrame({'VARIABLE': var,
+                                    'TYPE': str(df[var].dtypes),
+                                    'OBS': tot_rows,
+                                    'UNIQUE': df[var].nunique(),
+                                    'NANs': df[var].isna().sum(),
+                                    'MIN': np.datetime_as_string(val.min(), unit=date_format),
+                                    'MAX': np.datetime_as_string(val.max(), unit=date_format),
+                                    'MEDIAN': [k for k, v in mapping.items() if v == med_ind][0],
+                                    'PERC1': [k for k, v in mapping.items() if v == perc[0]][0],
+                                    'PERC5': [k for k, v in mapping.items() if v == perc[1]][0],
+                                    'PERC95': [k for k, v in mapping.items() if v == perc[2]][0],
+                                    'PERC99': [k for k, v in mapping.items() if v == perc[3]][0]
+                                   }, index = [0])
+        else:
+            add_row = pd.DataFrame({'VARIABLE': var,
+                            'TYPE': str(df[var].dtypes),
+                            'OBS': tot_rows,
+                            'UNIQUE': df[var].nunique(),
+                            'NANs': df[var].isna().sum()
+                       }, index = [0])
 
         date_stats = date_stats.append(add_row)
 
@@ -200,6 +214,15 @@ def stats_with_description(df, df_vardescr_path, col_to_lowercase=True):
     return df_stats
 
 
+def safe_last(x, missing_char = ''):
+    
+    x = x[x != missing_char]
+    if len(x) > 0:
+        return x.values[-1]
+    else:
+        return missing_char
+    
+    
 def match_gvkey(df, df_link_final, col_to_check = ['cusip', 'ticker']):
     
     mapping = {'cusip': 'cusip',       # map to df_link_final column names  {'current_table': df_link_final}
@@ -251,3 +274,158 @@ def match_gvkey(df, df_link_final, col_to_check = ['cusip', 'ticker']):
         print('\n- Multiple gvkey with shared values:', multiple_gvkey_check.shape[0])
     
     return df
+
+
+def merge_df(df_left, df_right, left_on_key = 'year', right_on_key = 'year', right_columns = ['year', 'value'],
+            duplicated_groupby_check = 'year', check_duplicates_right=True, warn=True):
+    
+    '''
+        right_columns: columns to keep when merging df_right
+        duplicated_groupby_check: column to groupby multiple gvkey so as to select most populated one and remove duplicates
+    '''
+
+    cc = 0
+    ref_tab = pd.DataFrame({'gvkey': [], 'ID': []})
+
+    # assign unique ID to multiple gvkey istances
+    df_mul = df_left[df_left.gvkey_tot > 1]
+    if df_mul.shape[0] > 0:
+        mul_list = np.unique(df_mul['gvkey'])
+
+        for el in mul_list:
+            ref_tab = ref_tab.append(pd.DataFrame({'gvkey': el, 'ID': str(cc)}))
+            cc += 1
+
+        check_multiple_assignment = ref_tab.groupby('gvkey').agg(count=('ID', lambda x: len(x)),
+                                        uniqueID=('ID', lambda x: list(x))).reset_index().query('count > 1')
+        if check_multiple_assignment.shape[0] > 0:
+            for index, row in check_multiple_assignment.iterrows():
+                ref_tab['ID'][ref_tab['ID'].isin(row['uniqueID'])] = min(row['uniqueID'])
+
+        ref_tab.drop_duplicates(inplace=True)
+
+    # append unique ID for single gvkey istances
+    sin_list = np.unique(df_left[df_left.gvkey_tot == 1]['gvkey'])
+    sin_list = list(itertools.chain(*sin_list))
+    t_sin = pd.DataFrame({'gvkey': sin_list, 'ID': ''})
+    t_sin = t_sin[~t_sin['gvkey'].isin(ref_tab['gvkey'])]
+    t_sin['ID'] = [str(x) for x in np.arange(cc, cc + len(t_sin))]
+    ref_tab=ref_tab.append(t_sin)
+    ref_tab = dict(zip(ref_tab['gvkey'], ref_tab['ID']))
+    
+    # map ID on df_left
+    df_left.insert(0, 'xxx_ID', df_left['gvkey'].apply(lambda x: np.unique([y for y in [ref_tab.get(i) for i in x] if y is not None])))
+    if np.unique([len(x) for x in df_left['xxx_ID']]).max() > 1:
+        print('##### warning: multiple unique ID mapped to gvkey - taken only first value on df_left')
+    df_left['xxx_ID'] = df_left['xxx_ID'].str[0]
+    
+    # remove duplicated unique ID xxx_ID for single gvkey_original
+    check_ID_left = df_left[['xxx_ID', 'gvkey_original']].drop_duplicates()
+    check_ID_left_dupl_gvkey = check_ID_left['gvkey_original'].value_counts().to_frame().reset_index().query('gvkey_original > 1')['index'].values
+    if len(check_ID_left_dupl_gvkey) > 0:
+        check_ID_left_dupl_ID = check_ID_left[check_ID_left['gvkey_original'].isin(check_ID_left_dupl_gvkey)]['xxx_ID'].values
+        row_counts=df_left.shape[0]
+        df_left = df_left.copy()[~df_left['xxx_ID'].isin(check_ID_left_dupl_ID)]
+        ref_tab = {k: v for k, v in ref_tab.items() if v not in check_ID_left_dupl_ID}
+        print('- Removed rows from df_left because of multiple unique ID on same gvkey_original:', row_counts - df_left.shape[0])
+    
+    # map ID on df_right
+    df_right = df_right[df_right['gvkey_tot'] > 0]
+    df_right.insert(0, 'xxx_ID', df_right['gvkey'].apply(lambda x: np.unique([y for y in [ref_tab.get(i) for i in x] if y is not None])))
+    if np.unique([len(x) for x in df_right['xxx_ID']]).max() > 1:
+        print('##### warning: multiple unique ID mapped to gvkey - taken only first value on df_right')
+    pd.options.mode.chained_assignment = None  # default='warn'
+    df_right.loc[:, ['xxx_ID']] = df_right['xxx_ID'].str[0]
+    pd.options.mode.chained_assignment = 'warn'  # default='warn'
+    df_right = df_right[~df_right['xxx_ID'].isnull()]
+
+    # solve duplicated rows (some company may have been split during the same year) in df_left
+    check_duplicates=df_left[['xxx_ID']+[duplicated_groupby_check]].groupby(['xxx_ID', duplicated_groupby_check]).size().reset_index().rename(columns={0:'val'})
+    check_duplicates=check_duplicates[check_duplicates['val'] > 1]
+    if check_duplicates.shape[0] > 0:
+        print('\n-- dropping duplicated values in df_left for "' + duplicated_groupby_check + '" - duplicates are evaluated according to "gvkey_original"')
+        index_to_remove = []
+        for id in check_duplicates['xxx_ID'].unique():
+            df_t=df_left.copy()[df_left['xxx_ID'] == id].copy()
+    #         df_t['gvkey']=df_t['gvkey'].apply(lambda x: '_'.join(x))
+            majority_gvkey=df_t['gvkey_original'].value_counts().to_frame().sort_values(by='gvkey_original', ascending=False).reset_index()
+            gvkey_to_keep=majority_gvkey.iloc[0]['index']
+            duplicated_val=df_t.groupby(duplicated_groupby_check).size().to_frame().reset_index().reset_index().rename(columns={0:'val'})
+            duplicated_val=duplicated_val[duplicated_val['val'] > 1][duplicated_groupby_check].values
+    #         allowed_gvkey=np.unique(df_t[~df_t[duplicated_groupby_check].isin(duplicated_val)]['gvkey_original'].values)
+    #         mask = (~df_t[duplicated_groupby_check].isin(duplicated_val)) | ((df_t['gvkey_original'].isin(allowed_gvkey)) & (df_t[duplicated_groupby_check].isin(duplicated_val)))
+            mask = (~df_t[duplicated_groupby_check].isin(duplicated_val)) | ((df_t['gvkey_original'] == gvkey_to_keep) & (df_t[duplicated_groupby_check].isin(duplicated_val)))
+            index_to_remove.extend(mask[mask==False].index)
+        df_left=df_left.drop(index_to_remove, axis=0)
+
+        check_duplicates=df_left[['xxx_ID']+[duplicated_groupby_check]].groupby(['xxx_ID', duplicated_groupby_check]).size().reset_index().rename(columns={0:'val'})
+        if check_duplicates[check_duplicates['val'] > 1].shape[0] > 0:
+            print('    #### duplicates still present')
+
+    # solve duplicated rows (some company may have been split during the same year) in df_right
+    if check_duplicates_right:
+        check_duplicates=df_right[['xxx_ID']+right_columns].groupby(['xxx_ID',duplicated_groupby_check]).size().reset_index().rename(columns={0:'val'})
+        check_duplicates=check_duplicates[check_duplicates['val'] > 1]
+        if check_duplicates.shape[0] > 0:
+            print('\n-- dropping duplicated values in df_right for "' + duplicated_groupby_check + '" - duplicates are evaluated according to "gvkey"')
+            index_to_remove = []
+            for id in check_duplicates['xxx_ID'].unique():
+                df_t=df_right.copy()[df_right['xxx_ID'] == id].copy()
+                df_t['gvkey']=df_t['gvkey'].str[0]
+                majority_gvkey=df_t['gvkey'].value_counts().to_frame().sort_values(by='gvkey', ascending=False).reset_index()
+                gvkey_to_keep=majority_gvkey.iloc[0]['index']
+                duplicated_val=df_t.groupby(duplicated_groupby_check).size().to_frame().reset_index().reset_index().rename(columns={0:'val'})
+                duplicated_val=duplicated_val[duplicated_val['val'] > 1][duplicated_groupby_check].values
+    #             allowed_gvkey=np.unique(df_t[~df_t[duplicated_groupby_check].isin(duplicated_val)]['gvkey'].values)
+    #             mask = (~df_t[duplicated_groupby_check].isin(duplicated_val)) | ((df_t['gvkey'].isin(allowed_gvkey)) & (df_t[duplicated_groupby_check].isin(duplicated_val)))
+                mask = (~df_t[duplicated_groupby_check].isin(duplicated_val)) | ((df_t['gvkey'] == gvkey_to_keep) & (df_t[duplicated_groupby_check].isin(duplicated_val)))
+                index_to_remove.extend(mask[mask==False].index)
+            df_right=df_right.drop(index_to_remove, axis=0)
+
+        check_duplicates=df_right[['xxx_ID']+[duplicated_groupby_check]].groupby(['xxx_ID', duplicated_groupby_check]).size().reset_index().rename(columns={0:'val'})
+        if check_duplicates[check_duplicates['val'] > 1].shape[0] > 0:
+            print('    #### duplicates still present')
+            
+    # merge dataset by "xxx_ID" and left_on_key / right_on_key
+    if type(left_on_key) != list:
+        left_on_key = [left_on_key]
+    if type(right_on_key) != list:
+        right_on_key = [right_on_key]
+    df_merge = df_left.copy()
+    df_merge = df_merge.merge(df_right[['xxx_ID']+right_columns], left_on=['xxx_ID']+left_on_key, right_on=['xxx_ID']+right_on_key, how='left')
+
+    if df_left.shape[0] != df_merge.shape[0] and warn:
+        print('##### warning: merged dataset has different number of rows (' + str(df_merge.shape[0]) +
+              ') with respect to df_left (' + str(df_left.shape[0]) + ')')
+
+#     df_merge.drop(columns=['xxx_ID'], inplace=True)
+    
+    return df_merge, ref_tab
+
+
+def find_consec_year(df, min_consecutive_years = 2, year_col = 'year', col_na_check = 'EPS_next_year'):
+    
+    lis = df.copy()[~df[col_na_check].isna()][year_col].values
+    df['consec_year'] = False
+    df['consec_year_group'] = -1
+    if len(lis) > 0:
+        consec_group = functools.reduce(lambda x,y : x[:-1]+[x[-1]+[y]] if (x[-1][-1]+1==y) else [*x,[y]], lis[1:] , [[lis[0]]] )
+        year_to_keep = [x for x in consec_group if len(x) >= min_consecutive_years]
+        year_groups = [[i] * len(year_to_keep[i]) for i in range(len(year_to_keep))]
+        year_to_keep = [x for y in year_to_keep for x in y]
+        year_groups = [x for y in year_groups for x in y]
+        df.loc[df[year_col].isin(year_to_keep), 'consec_year'] = True
+        df.loc[df[year_col].isin(year_to_keep), 'consec_year_group'] = year_groups
+
+    return df
+
+def safe_div(x, y):
+    
+    # 0/0 -> 0
+    if x == 0 and y == 0:
+        return 0
+    # num/0 -> sign(num)
+    elif y == 0:
+        return (x > 0) - (x < 0)
+    else:
+        return x / y
